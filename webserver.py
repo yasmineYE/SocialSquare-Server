d2e899a899df58d668a48681c237e85a182f6bf7
François Njeneza <njenezamed@gmail.com>
2016-01-07 09:30:43 +0100

    handle attributeError

--- webserver.py
+++ webserver.py
@@ -9,8 +9,16 @@ from crud import CRUD
 class SQHandler(BaseHTTPRequestHandler):
     
     def get(self,requestline, param):
-        pattern = re.compile(r'.*'+param+'=(?P<argument>\w*)&?.*')
-        return pattern.search(requestline).group('argument')
+        """
+        retrieve parameter from urls
+        """
+        value=None
+        try:
+            pattern = re.compile(r'.*'+param+'=(?P<argument>\w*)&?.*')
+            value = pattern.search(requestline).group('argument')
+        except AttributeError:
+            pass
+        return value
 
     def do_GET(self):
         crud = CRUD()
@@ -18,36 +26,22 @@ class SQHandler(BaseHTTPRequestHandler):
         print(self.requestline)
         #urls params
         requestline = self.requestline
+        resp = ''
 
         if (requestline.find("update_terminal")>=0):
-            #pattern = re.compile(r".*used=(?P<used>.*)&game=(?P<game>.*)&id=(?P<id>.*)\s+.*")
-            #results = pattern.search(requestline)
-            #used = results.group('used')
-            #game = results.group('game')
-            #id = results.group('id')
             used = self.get(requestline, "used")
             game = self.get(requestline, "game")
             id = self.get(requestline, "id")
             crud.updateTerminal(id,used, game)
 
         if (requestline.find("post_score")>=0):
-            #pattern = re.compile(r".*game=(?P<game>.*)&user=(?P<user>.*)&score=(?P<score>.*)\s+.*")
-            #results = pattern.search(requestline)
-            #game = results.group('game')
-            #user = results.group('user')
-            #score = results.group('score')
             game = self.get(requestline, "game")
             user = self.get(requestline, 'user')
             score = self.get(requestline, 'score')
             resp = (game,user,score)
 
         if (requestline.find("get_score")>=0):
-            #pattern = re.compile(r".*game=(?P<game>.*)\s+.*")
-            #results = pattern.search(requestline)
-            #game = results.group('game')
             game = self.get(requestline, "game")
-            print(game)
-            #resp = game
             resp = crud.getScore(game)
 
         self.send_response(200)
@@ -55,9 +49,6 @@ class SQHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         self.wfile.write(json.dumps(resp).encode("utf-8"))
-
-    def stop(self):
-        self.server.shutdown()
 
 if __name__ == "__main__":
     PORT = 8000
